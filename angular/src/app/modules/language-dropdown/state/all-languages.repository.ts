5b4ebc639d3c44c7e0c10fcc2dabdc437090d480
matviienko.d <danylo.matviienko@nixsolutions.com>
2022-06-06 15:56:41 +0300

    NJS-5258 removed activeId feature from allLanguageRepo

--- angular/src/app/modules/language-dropdown/state/all-languages.repository.ts
+++ angular/src/app/modules/language-dropdown/state/all-languages.repository.ts
@@ -1,5 +1,5 @@
 import { createStore } from '@ngneat/elf';
-import { selectAllEntities, setEntities, withActiveId, withEntities } from '@ngneat/elf-entities';
+import { selectAllEntities, setEntities, withEntities } from '@ngneat/elf-entities';
 import { AllLanguagesItem } from '@models/libre-translation';
 import { Injectable } from '@angular/core';
 import { Observable } from "rxjs";
@@ -7,7 +7,6 @@ import { Observable } from "rxjs";
 const languagesStore = createStore(
   { name: 'allLanguages' },
   withEntities<AllLanguagesItem, 'code'>({ idKey: 'code' }),
-  withActiveId('en')
 );
 
 @Injectable()
